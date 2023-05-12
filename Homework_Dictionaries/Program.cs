3f0aed2ee46f3e427c5c0c7092d76030a4d85be1
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-11 23:42:37 -0600

    code complete.

--- Homework_Dictionaries/Program.cs
+++ Homework_Dictionaries/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Create a Dictionary list of employees ID and the name that goes with the ID.
+// Fill in a few records.
+// Then ask the user for their ID and return their name.
+
+//create dictionary with filled records
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+//fill dictionary with records
+employees[134] = "Bobby Jackson";
+employees[784] = "Bilbo Bagpipe";
+employees[321] = "Corey Torey";
+employees[620] = "Larry Bones";
+
+//loop until user enters 0
+int id;
+do
+{
+    //ask user for their ID and store in a variable
+    Console.Write("What is your ID (0 to exit): ");
+    int.TryParse(Console.ReadLine(), out id);
+
+    //check if ID matches dictionary key and print to console
+    if (id != 0)
+    {
+        if (employees.ContainsKey(id))
+        {
+            Console.WriteLine($"Your name is {employees[id]}.");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect ID!");
+        }
+    }
+} while (id != 0);
